096c8337cd78f37cab6b094b6b848780de22584c
Michael <osajehmichael@gmail.com>
2022-08-29 16:18:07 +0100

    set theme transparent so background is determined by the terminal

--- lua/config/plugins/colourscheme.lua
+++ lua/config/plugins/colourscheme.lua
@@ -9,13 +9,15 @@ require("kanagawa").setup({
 	variablebuiltinStyle = { italic = true },
 	specialReturn = true, -- special highlight for the return keyword
 	specialException = true, -- special highlight for exception handling keywords
-	transparent = false, -- do not set background color
+	transparent = true, -- do not set background color
 	dimInactive = false, -- dim inactive window `:h hl-NormalNC`
 	globalStatus = true,
 	terminalColors = true,
 	colors = {},
 	overrides = {
 		FloatBorder = { bg = "NONE" },
+		NormalFloat = { bg = "NONE" },
+		TelescopeBorder = { bg = "NONE" },
 	},
 	theme = "default",
 })
