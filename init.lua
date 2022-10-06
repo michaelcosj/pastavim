2a7141d809640ab84bdfe6f8eb917b817c60eb93
michael <mikicosj@gmail.com>
2022-10-06 17:46:22 +0100

    put impatient at the very top

--- init.lua
+++ init.lua
@@ -1,2 +1,8 @@
+-- Safely call impatient to speed up loading plugins
+local ok, impatient = pcall(require, "impatient")
+if ok then
+  impatient.enable_profile()
+end
+
 require("config.core")
 require("config.plugins")
