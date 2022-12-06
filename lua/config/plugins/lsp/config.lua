7a98d51482eaf4d4aded969f3cffcd7f58b2cbdb
Michaelcosj <michaelcosj@gmail.com>
2022-12-06 23:55:35 +0100

    refactor and replace pyright with pylsp

--- lua/config/plugins/lsp/config.lua
+++ lua/config/plugins/lsp/config.lua
@@ -1,67 +1,70 @@
 ---@diagnostic disable: redefined-local
 local ok, lspconfig = pcall(require, "lspconfig")
 if not ok then
-	return
+  return
 end
 
 local ok, mason_lspconfig = pcall(require, "mason-lspconfig")
 if not ok then
-	return
+  return
 end
 
 local on_attach = function(client, bufnr)
-	-- Init mappings on attach
-	local handler = require("config.plugins.lsp.handler")
-	handler.init()
+  -- Init mappings on attach
+  local handler = require("config.plugins.lsp.handler")
+  handler.init()
 
-	-- Turn off formating for some servers
-	handler.lsp_keymaps(bufnr)
+  handler.lsp_keymaps(bufnr)
+  handler.diagnostics_keymaps()
 
-	handler.diagnostics_keymaps()
+  handler.fmt_on_save(client, bufnr)
 
-	handler.fmt_on_save(client, bufnr)
+  -- Auto show diagnostics in hover window
+  handler.auto_hover()
 
-	-- Auto show diagnostics in hover window
-	handler.auto_hover()
+  -- codecontext on winbar
+  local ok, navic = pcall(require, "nvim-navic")
+  if ok then
+    navic.attach(client, bufnr)
+  end
 
-	-- codecontext on winbar
-	local ok, navic = pcall(require, "nvim-navic")
-	if ok then
-		navic.attach(client, bufnr)
-	end
-
-	vim.notify("client " .. client.name .. " attached")
+  vim.notify("client " .. client.name .. " attached")
 end
 
 -- Autocomplete
-local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+-- Servers to install
+local servers = {
+  "clangd",
+  "bashls",
+  "cssls",
+  "gopls",
+  "hls",
+  "html",
+  "jsonls",
+  "marksman",
+  -- "pyright",
+  "pylsp",
+  "sumneko_lua",
+  "tsserver",
+  "rust_analyzer",
+}
 
 mason_lspconfig.setup({
-	ensure_installed = {
-		"clangd",
-		"bashls",
-		"cssls",
-		"gopls",
-		"html",
-		"jsonls",
-		"marksman",
-		"pyright",
-		"sumneko_lua",
-		"tsserver",
-		"rust_analyzer",
-	},
+  ensure_installed = servers,
 })
 
-for _, server in pairs(mason_lspconfig.get_installed_servers()) do
-	local server_opts = {
-		on_attach = on_attach,
-		capabilities = capabilities,
-	}
+for _, server in pairs(servers) do
+  local opts = {
+    on_attach = on_attach,
+    capabilities = capabilities,
+  }
 
-	local has_custom_opts, server_custom_opts = pcall(require, "config.plugins.lsp.settings." .. server)
-	if has_custom_opts then
-		server_opts = vim.tbl_deep_extend("force", server_opts, server_custom_opts)
-	end
+  local has_custom_opts, custom_opts = pcall(require, "config.plugins.lsp.settings." .. server)
+  if has_custom_opts then
+    opts = vim.tbl_deep_extend("force", opts, custom_opts)
+  end
 
-	lspconfig[server].setup(server_opts)
+  lspconfig[server].setup(opts)
 end
