051a8db0539474ae908c9b7b3de0e26404c0d0e9
Michaelcosj <michaelcosj@gmail.com>
2022-12-06 22:54:53 +0100

    Refactor keymaps

--- lua/config/core/keymaps.lua
+++ lua/config/core/keymaps.lua
@@ -3,16 +3,31 @@ local keymap = require("config.core.utils").keymap
 
 --Remap space as leader key
 keymap("", "<Space>", "<Nop>", {})
-
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
--- Normal --
+-- Core Keymaps {{
 -- Better window navigation
 keymap("n", "<C-h>", "<C-w>h", {})
 keymap("n", "<C-j>", "<C-w>j", {})
 keymap("n", "<C-k>", "<C-w>k", {})
 keymap("n", "<C-l>", "<C-w>l", {})
+
+-- Move text up and down normal mode
+keymap("n", "<A-j>", "<Esc>:m .+1<CR>==g", {})
+keymap("n", "<A-k>", "<Esc>:m .-2<CR>==g", {})
+
+-- Move text up and down visual mode
+keymap("v", "<A-j>", ":m .+1<CR>==", {})
+keymap("v", "<A-k>", ":m .-2<CR>==", {})
+
+-- Move text up and down visual block
+keymap("x", "<A-j>", ":move '>+1<CR>gv-gv", {})
+keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", {})
+
+-- Indent visual selection easily
+keymap("v", "<", "<gv", {})
+keymap("v", ">", ">gv", {})
 
 -- Resize with arrows
 keymap("n", "<C-Up>", ":resize -2<CR>", {})
@@ -20,35 +35,9 @@ keymap("n", "<C-Down>", ":resize +2<CR>", {})
 keymap("n", "<C-Left>", ":vertical resize -2<CR>", {})
 keymap("n", "<C-Right>", ":vertical resize +2<CR>", {})
 
--- Navigate buffers
--- NOTE: using bufdelete plugin instead
--- keymap("n", "<S-l>", ":bnext<CR>", {})
--- keymap("n", "<S-h>", ":bprevious<CR>", {})
-
--- Move text up and down
-keymap("n", "<A-j>", "<Esc>:m .+1<CR>==gi", {})
-keymap("n", "<A-k>", "<Esc>:m .-2<CR>==gi", {})
-
 -- Saving files
 keymap("n", "<leader>w", ":w<CR>", {})
 
--- Insert --
--- Press jk fast to exit insert mode
--- keymap("i", "jk", "<ESC>", {})
-
--- Visual --
--- Stay in indent mode
-keymap("v", "<", "<gv", {})
-keymap("v", ">", ">gv", {})
-
--- Move text up and down
-keymap("v", "<A-j>", ":m .+1<CR>==", {})
-keymap("v", "<A-k>", ":m .-2<CR>==", {})
+-- Better paste? (NOTE Not sure what exactly it does though)
 keymap("v", "p", '"_dP', {})
-
--- Visual Block --
--- Move text up and down
-keymap("x", "J", ":move '>+1<CR>gv-gv", {})
-keymap("x", "K", ":move '<-2<CR>gv-gv", {})
-keymap("x", "<A-j>", ":move '>+1<CR>gv-gv", {})
-keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", {})
+-- }}
