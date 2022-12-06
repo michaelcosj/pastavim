419c3fc4969769fe3b814f0d13198245417ae245
Michaelcosj <michaelcosj@gmail.com>
2022-12-06 22:16:16 +0100

    added autocmd to auto format pascal code files

--- lua/config/core/autocmds.lua
+++ lua/config/core/autocmds.lua
@@ -5,6 +5,7 @@ vim.cmd([[
     autocmd TextYankPost * silent!lua require('vim.highlight').on_yank({higroup = 'Visual', timeout = 200}) 
     autocmd BufWinEnter * :set formatoptions-=cro
     autocmd FileType qf set nobuflisted
+    autocmd BufWritePost *.pas !ptop % % 
   augroup end
 
   augroup _git
